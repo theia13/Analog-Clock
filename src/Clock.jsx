ec3959549535f3c27e5a94de0d44b28cb1733af3
theia13 <115384349+theia13@users.noreply.github.com>
2024-07-27 14:23:40 +0530

    dark mode to light mode

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,13 +3,28 @@ import { useState, useEffect } from "react";
 export default function Clock() {
   const [currenttime, setCurrentTime] = useState(new Date());
 
+  const [currentMode, setcurrentMode] = useState("dark");
+
+  const handleClick = () => {
+    setcurrentMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
+    console.log("Body was clicked");
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
+    document.body.addEventListener("click", handleClick);
 
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+      document.body.removeEventListener("click", handleClick);
+    };
   }, []);
+
+  useEffect(() => {
+    document.body.className = currentMode;
+  }, [currentMode]);
 
   const hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
@@ -17,15 +32,10 @@ export default function Clock() {
   const minutes = currenttime.getMinutes() * 6;
   const hourstime = currenttime.getHours() * 30;
 
-  // console.log(seconds);
-  // console.log(minutes);
-  // console.log(currenttime)
-  // console.log(hourstime);
-
   return (
     <>
       <div className="clock">
-        <div className="marks">
+        <div className={`marks ${currentMode}`} onClick={handleClick}>
           <div className="hour-mark"></div>
           <div className="hour-mark"></div>
           <div className="hour-mark"></div>
@@ -39,19 +49,25 @@ export default function Clock() {
           <div className="hour-mark"></div>
           <div className="hour-mark"></div>
         </div>
-        <div className="clock-frame">
+        <div className={`clock-frame ${currentMode}`} onClick={handleClick}>
           {hours.map((hour, index) => (
-            <div key={index} className="hours">
+            <div
+              key={index}
+              className={`hours ${currentMode}`}
+              onClick={handleClick}
+            >
               {hour}
             </div>
           ))}
         </div>
         <div
-          className="hour-hand"
+          className={`hour-hand ${currentMode}`}
+          onClick={handleClick}
           style={{ transform: `rotate(${hourstime}deg) translateX(-50%)` }}
         ></div>
         <div
-          className="minute-hand"
+          className={`minute-hand ${currentMode}`}
+          onClick={handleClick}
           style={{
             transform: `rotate(${minutes}deg) translateX(-50%)`,
           }}
